27b256299963e026ddae67b2a4538e1345cb1717
Sergey Zinoviev <servzin@gmail.com>
2018-04-06 15:47:12 +0300

    Create User model

---
+++ db/migrate/20180323130823_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.decimal :balance, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
