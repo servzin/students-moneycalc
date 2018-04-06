dbeb0aed360efb195250dc0f33313a10774b17b8
Sergey Zinoviev <servzin@gmail.com>
2018-04-06 16:19:21 +0300

    Email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /[\wа-я\.\+]+@([\wа-я]+\.?)+/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password_digest, presence: true
 
   before_save { self.email = email.downcase }
