0ec0736534c084bdc1e5677e8c547b57c25f00e9
Sergey Zinoviev <servzin@gmail.com>
2018-04-13 16:54:45 +0300

    Generate devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 class User < ApplicationRecord
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   VALID_EMAIL_REGEX = /[\wа-я\.\+]+@([\wа-я]+\.?)+/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password_digest, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'users#index'
   resources :users
   get '/all_users', to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
